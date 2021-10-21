e9d308b42f44b594bb3bc0e307d561ab2bc00bf5
YJYJLee <jane001356@gmail.com>
2021-10-21 23:31:21 +0900

    wasp spdx license add

--- core/src/main/scala/org/apache/spark/rdd/PartitionerAwareUnionRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/PartitionerAwareUnionRDD.scala
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Copyright 2021 Seoul National University
+#
+# SPDX-License-Identifier: MIT AND Apache-2.0
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
